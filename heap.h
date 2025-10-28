9e98f38c7456beb5a12f7b4a8856abf27b308185
klong2558-sys <klong2558@sdsu.edu>
2025-10-28 15:40:59 -0700

    Features: Implement push, pop, upheap and downheap in heap header

--- heap.h
+++ heap.h
@@ -1,6 +1,3 @@
-//
-// Created by Manju Muralidharan on 10/19/25.
-//
 
 #ifndef HEAP_H
 #define HEAP_H
@@ -15,21 +12,45 @@ struct MinHeap {
     MinHeap() { size = 0; }
 
     void push(int idx, int weightArr[]) {
-        // TODO: insert index at end of heap, restore order using upheap()
+        data[size]=idx;
+        upheap(size, weightArr);
+        size++;
     }
 
     int pop(int weightArr[]) {
-        // TODO: remove and return smallest index
-        // Replace root with last element, then call downheap()
-        return -1; // placeholder
+
+        if (size == 0) return -1;  // heap is empty
+        int minIndex = data[0];
+        data[0] = data[size - 1];
+        size--;
+        downheap(0, weightArr);
     }
 
     void upheap(int pos, int weightArr[]) {
-        // TODO: swap child upward while smaller than parent
+
+        while (pos > 0) {
+            int parent = (pos - 1) / 2;
+
+            if (weightArr[data[pos]] < weightArr[data[parent]]) {
+                swap(data[pos], data[parent]);
+                pos = parent;
+            }else {
+                break;
+            }
+        }
     }
 
     void downheap(int pos, int weightArr[]) {
-        // TODO: swap parent downward while larger than any child
+        while (pos < size) {
+            int child = 2 * pos;
+
+            if (weightArr[child] < weightArr[pos]) {
+                swap(data[pos], data[child]);
+                pos = child;
+            }else {
+                break;
+            }
+        }
     }
 };
 
